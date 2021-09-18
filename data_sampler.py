99fc57b0664842894952ad8e58609ac40d9906ba
PENGUINLOVEKOALA <wy17892022@gmail.com>
2021-09-18 21:25:17 +0900

    penguin

--- data_sampler.py
+++ data_sampler.py
@@ -1,25 +1,28 @@
 from glob import glob
 from data_loading_functions import CSVLoader
 from params import Parameters
+import sys
 
-class DataSampler:
+class AtomDataSampler:
     def __init__(self):
-        self.atom_dir = "../data/atomic/"
-        self.conceptnet_dir = "../data/conceptnet/"
+        self.target_name = Parameters.target_name
+        self.target_type = Parameters.target_type
+        self.name_type_dict = Parameters.name_type2dir
+        self.name_type = f"{self.target_name}|{self.target_type}"
+        self.dir_ = Parameters.atom_dir
+        self.dir_ += self.name_type_dict[self.name_type]
 
     def __call__(self):
-        csv_files = self.get_csv_files(self.atom_dir)
-        print("koala")
-        print(csv_files)
-
-    def get_csv_files(self, dir_):
-        files = glob(dir_ + "*.csv")
-        return files
+        csv_loader = CSVLoader(self.dir_)
+        content = csv_loader()
+        print(len(content))
+        print(type(content))
+        sys.exit()
 
     def koala():
         pass
 
 
 if __name__ == "__main__":
-    data_sampler = DataSampler()
-    data_sampler()
+    sampler = AtomDataSampler()
+    sampler()
--- params.py
+++ params.py
@@ -4,8 +4,10 @@ class Parameters:
     target_name = "atom"
     target_type = "test"
     random_sampling = True
+    atom_dir = "../data/atomic/"
+    conceptnet_dir = "../data/conceptnet/"
     save_to_file = True
-    save_dir = f"../data/samples/{target_data_name}_{target_data_type}_sample.txt"
+    save_dir = f"../data/samples/{target_name}_{target_type}_sample.txt"
     name_type2dir = {
             "atom|train": "v4_atomic_trn.csv",
             "atom|dev": "v4_atomic_trn.csv",
